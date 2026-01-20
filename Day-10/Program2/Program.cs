3c24e460f8bf2a97010e0de90b432c6c10ba074a
Akhila <durgaakhila.k@gmail.com>
2026-01-20 16:33:35 +0530

    Day-10: Encryption

---
+++ Day-10/Program2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace text_to_speech_convertor_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           SpeechSynthesizer synth = new SpeechSynthesizer();
+           synth.Speak("Hello, welcome to Text to Speech conversion app"); 
+        }
+    }
+}
